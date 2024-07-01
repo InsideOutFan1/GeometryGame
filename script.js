1799e3d8dc70d2a08a9841d91feeeee403d75a7b
InsideOutFan1 <166262263+InsideOutFan1@users.noreply.github.com>
2024-07-01 12:20:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var buildUrl = "https://geometrylite.github.io/Build_geo_jump";
+		var loaderUrl = buildUrl + "/geometry-jump.loader.js";
+		var config = {
+		dataUrl: buildUrl + "/geometry-jump.data.unityweb",
+		frameworkUrl: buildUrl + "/geometry-jump.framework.js.unityweb",
+		codeUrl: buildUrl + "/geometry-jump.wasm.unityweb",
+				streamingAssetsUrl: "StreamingAssets",
+		companyName: "1games.io",
+		productName: "Geometry Jump",
+		productVersion: "1.1",
+		}
